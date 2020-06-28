a32f218fc1f0f9e1ea9e4aeaa1f356d3aecbf834
Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
2020-06-28 09:01:19 +0200

    Startd the handout intermediate

--- scripts/sine.py
+++ scripts/sine.py
@@ -5,12 +5,52 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-X = np.linspace(0, 10*np.pi, 1000)
+X = np.linspace(0.1, 10*np.pi, 10000)
 Y = np.sin(X)
 
-plt.figure = plt.figure(figsize=(8,2))
-plt.plot(X, Y, color="orange", linewidth=2)
+# fig = plt.figure(figsize=(8,2))
+# plt.plot(X, Y, color="orange", linewidth=2)
 # plt.xticks([]), plt.yticks([])
 # plt.tight_layout()
-plt.savefig("../figures/sine.pdf", dpi=100)
+# plt.savefig("../figures/sine.pdf", dpi=100)
+
+# fig = plt.figure(figsize=(7,1.5))
+# plt.plot(X, Y, "C1o:", markevery=500,  mec="1.0", lw=2, ms=8.5, mew=2)
+# # plt.xticks([]), plt.yticks([])
+# plt.ylim(-1.5, 1.5)
+# plt.tight_layout()
+# plt.savefig("../figures/sine-marker.pdf", dpi=100)
+
+# fig, ax = plt.subplots(figsize=(7,1.5))
+# ax.set_xscale("log")
+# ax.plot(X, Y, "-")
+# plt.plot(X, Y, "C1o-", markevery=500,  mec="1.0", lw=2, ms=8.5, mew=2)
+# #plt.xticks([]), plt.yticks([])
+# plt.ylim(-1.5, 1.5)
+# plt.tight_layout()
+# plt.savefig("../figures/sine-logscale.pdf", dpi=100)
+
+
+fig = plt.figure(figsize=(7,1.5))
+plt.plot(X, Y, "C1", lw=2)
+plt.fill_betweenx([-1.5,1.5],[0],[2*np.pi], color=".9")
+plt.text(0, -1, r" Period $\Phi$")
+# plt.xticks([]), plt.yticks([])
+plt.ylim(-1.5, 1.5)
+plt.tight_layout()
+plt.savefig("../figures/sine-period.pdf", dpi=100)
 plt.show()
+
+# X = np.linspace(0, 2*np.pi, 10000)
+# Y = np.sin(10*X)
+
+# fig = plt.figure(figsize=(3,3))
+# ax = plt.subplot(projection="polar")
+# ax.set_rorigin(-3)
+# ax.set_xticks(np.linspace(0, 2*np.pi, 8+1 ))
+# ax.set_xticklabels([])
+# ax.set_yticklabels([])
+# plt.ylim(-1.5, 1.5)
+# plt.plot(X, Y, "C1")
+# plt.savefig("../figures/sine-polar.pdf", dpi=100)
+# plt.show()
---
+++ scripts/tick-locators-single.py
@@ -0,0 +1,39 @@
+# ----------------------------------------------------------------------------
+# Title:   Scientific Visualisation - Python & Matplotlib
+# Author:  Nicolas P. Rougier
+# License: BSD
+# ----------------------------------------------------------------------------
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+# Setup a plot such that only the bottom spine is shown
+def setup(ax):
+    ax.spines['right'].set_color('none')
+    ax.spines['left'].set_color('none')
+    ax.yaxis.set_major_locator(ticker.NullLocator())
+    ax.spines['top'].set_color('none')
+    ax.xaxis.set_ticks_position('bottom')
+    ax.tick_params(which='major', width=1.00)
+    ax.tick_params(which='major', length=5)
+    ax.tick_params(which='minor', width=0.75)
+    ax.tick_params(which='minor', length=2.5)
+    ax.set_xlim(0, 5)
+    ax.set_ylim(0, 1)
+    ax.patch.set_alpha(0.0)
+
+
+fig = plt.figure(figsize=(5, .5))
+fig.patch.set_alpha(0.0)
+n = 1
+
+fontsize = 18
+family = "Source Code Pro"
+
+# Null Locator
+ax = plt.subplot(n, 1, 1)
+setup(ax)
+ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+ax.xaxis.set_minor_locator(ticker.MultipleLocator(.1))
+plt.savefig("../figures/tick-multiple-locator.pdf", transparent=True)
+plt.show()
