859d4527d43f6c70a2d7f2ce909fdcb35e4768af
Jamal Mustafa <jimustafa@gmail.com>
2021-10-23 22:05:17 -0700

    refactor arrow styles
    - create arrow styles within subplots
    - allows for easy reshaping of the grid of arrow styles

--- scripts/annotation-arrow-styles.py
+++ scripts/annotation-arrow-styles.py
@@ -7,11 +7,12 @@ def demo_con_style(ax, connectionstyle):
             family="Source Code Pro",
             transform=ax.transAxes, ha="left", va="top", size="x-small")
 
-fig, ax = plt.subplots(figsize=(4, 2.5), frameon=False)
-ax.axis("off")
-for i,style in enumerate(mpatches.ArrowStyle.get_styles()):
-    x0, y0 = 5 + 5*(i%3), -(i//3)
-    x1, y1 = 1 + 5*(i%3), -(i//3)
+(fig, axes) = plt.subplots(5, 3, figsize=(4, 2.5), frameon=False)
+for ax in axes.flatten():
+    ax.axis("off")
+for i,(ax,style) in enumerate(zip(axes.flatten(), mpatches.ArrowStyle.get_styles())):
+    x0, y0 = 0.8, 0.5
+    x1, y1 = 0.2, 0.5
     ax.plot([x0, x1], [y0, y1], ".", color="0.25")
     ax.annotate("",
                 xy=(x0, y0), xycoords='data',
@@ -22,6 +23,7 @@ for i,style in enumerate(mpatches.ArrowStyle.get_styles()):
                                 patchA=None, patchB=None,
                                 connectionstyle="arc3,rad=0"))
     ax.text( (x1+x0)/2, y0-0.2, style,
+             transform=ax.transAxes,
              family = "Source Code Pro", ha="center", va="top")
 
 plt.savefig("../figures/annotation-arrow-styles.pdf")
