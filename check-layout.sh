f02c0cd2e32e0a42c79476f0724bf8302952f925
Jamal Mustafa <jimustafa@users.noreply.github.com>
2021-10-24 09:46:36 -0700

    update check-layout.sh
    Co-authored-by: Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>

--- check-layout.sh
+++ check-layout.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+#
+# Check that a given pdf has a certain number of pages.
+# Usage:
+#   check-num-pages.sh [pdffile] [num_pages]
 
 set -x
-
-[[ "$(pdfinfo $1 | grep Pages | awk '{print $2}')" == "$2" ]] || exit 1
+# pdffile=$1
+# num_pages=$1
+[[ "$(pdfinfo $pdffile | grep Pages | awk '{print $2}')" == "$num_pages" ]] || exit 1
