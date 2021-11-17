d09834595a6958cd838620d66753569ec777c3db
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2021-11-17 23:14:15 +0100

    Apply PEP-8 compatible code formatting
    While not visible in the cheatsheet itself, we should still adhere
    to common best practices, to be a role model for potential contributors.

--- scripts/advanced-plots.py
+++ scripts/advanced-plots.py
@@ -8,21 +8,20 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-fig = plt.figure(figsize=(0.4,0.4))
+fig = plt.figure(figsize=(0.4, 0.4))
 mpl.rcParams['axes.linewidth'] = 0.5
 mpl.rcParams['xtick.major.size'] = 0.0
 mpl.rcParams['ytick.major.size'] = 0.0
 d = 0.01
-ax = fig.add_axes([d,d,1-2*d,1-2*d])
-
+ax = fig.add_axes([d, d, 1-2*d, 1-2*d])
 
 # Step plot
 # -----------------------------------------------------------------------------
 X = np.linspace(0, 10, 16)
-Y = 4+2*np.sin(2*X)
+Y = 4 + 2*np.sin(2*X)
 ax.step(X, Y, color="C1", linewidth=0.75)
-ax.set_xlim(0, 8), ax.set_xticks(np.arange(1,8))
-ax.set_ylim(0, 8), ax.set_yticks(np.arange(1,8))
+ax.set_xlim(0, 8), ax.set_xticks(np.arange(1, 8))
+ax.set_ylim(0, 8), ax.set_yticks(np.arange(1, 8))
 ax.grid(linewidth=0.125)
 plt.savefig("../figures/advanced-step.pdf")
 ax.clear()
@@ -30,37 +29,36 @@ ax.clear()
 # Violin plot
 # -----------------------------------------------------------------------------
 np.random.seed(10)
-D = np.random.normal((3,5,4), (0.75, 1.00, 0.75), (200,3))
-VP = ax.violinplot(D, [2,4,6], widths=1.5,
+D = np.random.normal((3, 5, 4), (0.75, 1.00, 0.75), (200, 3))
+VP = ax.violinplot(D, [2, 4, 6], widths=1.5,
                    showmeans=False, showmedians=False, showextrema=False)
 for body in VP['bodies']:
     body.set_facecolor('C1')
     body.set_alpha(1)
-ax.set_xlim(0, 8), ax.set_xticks(np.arange(1,8))
-ax.set_ylim(0, 8), ax.set_yticks(np.arange(1,8))
+ax.set_xlim(0, 8), ax.set_xticks(np.arange(1, 8))
+ax.set_ylim(0, 8), ax.set_yticks(np.arange(1, 8))
 ax.set_axisbelow(True)
 ax.grid(linewidth=0.125)
 plt.savefig("../figures/advanced-violin.pdf")
 ax.clear()
 
-
 # Boxplot
 # -----------------------------------------------------------------------------
 np.random.seed(10)
-D = np.random.normal((3,5,4), (1.25, 1.00, 1.25), (100,3))
-VP = ax.boxplot(D, positions=[2,4,6], widths=1.5, patch_artist=True,
+D = np.random.normal((3, 5, 4), (1.25, 1.00, 1.25), (100, 3))
+VP = ax.boxplot(D, positions=[2, 4, 6], widths=1.5, patch_artist=True,
                 showmeans=False, showfliers=False,
-                medianprops = {"color": "white",
-                               "linewidth": 0.25},
-                boxprops = {"facecolor": "C1",
-                            "edgecolor": "white",
-                            "linewidth": 0.25},
-                whiskerprops = {"color": "C1",
-                                "linewidth": 0.75},
-                capprops = {"color": "C1",
-                            "linewidth": 0.75})
-ax.set_xlim(0, 8), ax.set_xticks(np.arange(1,8))
-ax.set_ylim(0, 8), ax.set_yticks(np.arange(1,8))
+                medianprops={"color": "white",
+                             "linewidth": 0.25},
+                boxprops={"facecolor": "C1",
+                          "edgecolor": "white",
+                          "linewidth": 0.25},
+                whiskerprops={"color": "C1",
+                              "linewidth": 0.75},
+                capprops={"color": "C1",
+                          "linewidth": 0.75})
+ax.set_xlim(0, 8), ax.set_xticks(np.arange(1, 8))
+ax.set_ylim(0, 8), ax.set_yticks(np.arange(1, 8))
 ax.set_axisbelow(True)
 ax.grid(linewidth=0.125)
 plt.savefig("../figures/advanced-boxplot.pdf")
@@ -69,13 +67,13 @@ ax.clear()
 # Barbs plot
 # -----------------------------------------------------------------------------
 np.random.seed(1)
-X = [[2,4,6]]
-Y = [[1.5,3,2]]
-U = -np.ones((1,3))*0
-V = -np.ones((1,3))*np.linspace(50,100,3)
-ax.barbs(X,Y,U,V, barbcolor="C1", flagcolor="C1", length=5, linewidth=0.5)
-ax.set_xlim(0, 8), ax.set_xticks(np.arange(1,8))
-ax.set_ylim(0, 8), ax.set_yticks(np.arange(1,8))
+X = [[2, 4, 6]]
+Y = [[1.5, 3, 2]]
+U = -np.ones((1, 3)) * 0
+V = -np.ones((1, 3)) * np.linspace(50, 100, 3)
+ax.barbs(X, Y, U, V, barbcolor="C1", flagcolor="C1", length=5, linewidth=0.5)
+ax.set_xlim(0, 8), ax.set_xticks(np.arange(1, 8))
+ax.set_ylim(0, 8), ax.set_yticks(np.arange(1, 8))
 ax.set_axisbelow(True)
 ax.grid(linewidth=0.125)
 plt.savefig("../figures/advanced-barbs.pdf")
@@ -84,11 +82,12 @@ ax.clear()
 # Event plot
 # -----------------------------------------------------------------------------
 np.random.seed(1)
-X = [2,4,6]
+X = [2, 4, 6]
 D = np.random.gamma(4, size=(3, 50))
-ax.eventplot(D, colors="C1", orientation="vertical", lineoffsets=X, linewidth=0.25)
-ax.set_xlim(0, 8), ax.set_xticks(np.arange(1,8))
-ax.set_ylim(0, 8), ax.set_yticks(np.arange(1,8))
+ax.eventplot(D, colors="C1", orientation="vertical", lineoffsets=X,
+             linewidth=0.25)
+ax.set_xlim(0, 8), ax.set_xticks(np.arange(1, 8))
+ax.set_ylim(0, 8), ax.set_yticks(np.arange(1, 8))
 ax.set_axisbelow(True)
 ax.grid(linewidth=0.125)
 plt.savefig("../figures/advanced-event.pdf")
@@ -97,28 +96,27 @@ ax.clear()
 # Errorbar plot
 # -----------------------------------------------------------------------------
 np.random.seed(1)
-X = [2,4,6]
-Y = [4,5,4]
+X = [2, 4, 6]
+Y = [4, 5, 4]
 E = np.random.uniform(0.5, 1.5, 3)
 ax.errorbar(X, Y, E, color="C1", linewidth=0.75, capsize=1)
-ax.set_xlim(0, 8), ax.set_xticks(np.arange(1,8))
-ax.set_ylim(0, 8), ax.set_yticks(np.arange(1,8))
+ax.set_xlim(0, 8), ax.set_xticks(np.arange(1, 8))
+ax.set_ylim(0, 8), ax.set_yticks(np.arange(1, 8))
 ax.set_axisbelow(True)
 ax.grid(linewidth=0.125)
 plt.savefig("../figures/advanced-errorbar.pdf")
 ax.clear()
 
-
 # Hexbin plot
 # -----------------------------------------------------------------------------
 np.random.seed(1)
-X = np.random.uniform(1.5,6.5,100)
-Y = np.random.uniform(1.5,6.5,100)
-C = np.random.uniform(0,1,10000)
+X = np.random.uniform(1.5, 6.5, 100)
+Y = np.random.uniform(1.5, 6.5, 100)
+C = np.random.uniform(0, 1, 10000)
 ax.hexbin(X, Y, C, gridsize=4, linewidth=0.25, edgecolor="white",
           cmap=plt.get_cmap("Wistia"), alpha=1.0)
-ax.set_xlim(0, 8), ax.set_xticks(np.arange(1,8))
-ax.set_ylim(0, 8), ax.set_yticks(np.arange(1,8))
+ax.set_xlim(0, 8), ax.set_xticks(np.arange(1, 8))
+ax.set_ylim(0, 8), ax.set_yticks(np.arange(1, 8))
 ax.set_axisbelow(True)
 ax.grid(linewidth=0.125)
 plt.savefig("../figures/advanced-hexbin.pdf")
@@ -127,10 +125,10 @@ ax.clear()
 # Hist plot
 # -----------------------------------------------------------------------------
 np.random.seed(1)
-X = 4 + np.random.normal(0,1.5,200)
-ax.hist(X, bins=8, facecolor="C1", linewidth=0.25, edgecolor="white",)
-ax.set_xlim(0, 8), ax.set_xticks(np.arange(1,8))
-ax.set_ylim(0, 80), ax.set_yticks(np.arange(1,80,10))
+X = 4 + np.random.normal(0, 1.5, 200)
+ax.hist(X, bins=8, facecolor="C1", linewidth=0.25, edgecolor="white")
+ax.set_xlim(0, 8), ax.set_xticks(np.arange(1, 8))
+ax.set_ylim(0, 80), ax.set_yticks(np.arange(1, 80, 10))
 ax.set_axisbelow(True)
 ax.grid(linewidth=0.125)
 plt.savefig("../figures/advanced-hist.pdf")
@@ -143,8 +141,8 @@ Y = np.random.uniform(-4, 4, 250)
 X = np.random.uniform(-4, 4, 250)
 ax.xcorr(X, Y, usevlines=True, maxlags=6, normed=True, lw=1,
          color="C1")
-ax.set_xlim(-8, 8), ax.set_xticks(np.arange(-8,8,2))
-ax.set_ylim(-.25, .25), ax.set_yticks(np.linspace(-.25,.25,9))
+ax.set_xlim(-8, 8), ax.set_xticks(np.arange(-8, 8, 2))
+ax.set_ylim(-.25, .25), ax.set_yticks(np.linspace(-.25, .25, 9))
 ax.set_axisbelow(True)
 ax.grid(linewidth=0.125)
 plt.savefig("../figures/advanced-xcorr.pdf")
--- scripts/basic-plots.py
+++ scripts/basic-plots.py
@@ -8,32 +8,32 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-fig = plt.figure(figsize=(0.4,0.4))
+fig = plt.figure(figsize=(0.4, 0.4))
 mpl.rcParams['axes.linewidth'] = 0.5
 mpl.rcParams['xtick.major.size'] = 0.0
 mpl.rcParams['ytick.major.size'] = 0.0
 d = 0.01
-ax = fig.add_axes([d,d,1-2*d,1-2*d])
+ax = fig.add_axes([d, d, 1 - 2 * d, 1 - 2 * d])
 
 # Basic line plot
 # -----------------------------------------------------------------------------
 X = np.linspace(0, 10, 100)
-Y = 4+2*np.sin(2*X)
+Y = 4 + 2*np.sin(2*X)
 ax.plot(X, Y, color="C1", linewidth=0.75)
-ax.set_xlim(0, 8), ax.set_xticks(np.arange(1,8))
-ax.set_ylim(0, 8), ax.set_yticks(np.arange(1,8))
+ax.set_xlim(0, 8), ax.set_xticks(np.arange(1, 8))
+ax.set_ylim(0, 8), ax.set_yticks(np.arange(1, 8))
 ax.grid(linewidth=0.125)
 plt.savefig("../figures/basic-plot.pdf")
 ax.clear()
 
+# Basic line plot (color)blaPwd
 
-# Basic line plot (color)
 # -----------------------------------------------------------------------------
 X = np.linspace(0, 10, 100)
-Y = 4+2*np.sin(2*X)
+Y = 4 + 2 * np.sin(2 * X)
 ax.plot(X, Y, color="black", linewidth=0.75)
-ax.set_xlim(0, 8), ax.set_xticks(np.arange(1,8))
-ax.set_ylim(0, 8), ax.set_yticks(np.arange(1,8))
+ax.set_xlim(0, 8), ax.set_xticks(np.arange(1, 8))
+ax.set_ylim(0, 8), ax.set_yticks(np.arange(1, 8))
 ax.grid(linewidth=0.125)
 plt.savefig("../figures/basic-plot-color.pdf")
 ax.clear()
@@ -41,12 +41,12 @@ ax.clear()
 # Basic scatter plot
 # -----------------------------------------------------------------------------
 np.random.seed(3)
-X = 4+np.random.normal(0, 1.25, 24)
-Y = 4+np.random.normal(0, 1.25, len(X))
+X = 4 + np.random.normal(0, 1.25, 24)
+Y = 4 + np.random.normal(0, 1.25, len(X))
 ax.scatter(X, Y, 5, zorder=10,
            edgecolor="white", facecolor="C1", linewidth=0.25)
-ax.set_xlim(0, 8), ax.set_xticks(np.arange(1,8))
-ax.set_ylim(0, 8), ax.set_yticks(np.arange(1,8))
+ax.set_xlim(0, 8), ax.set_xticks(np.arange(1, 8))
+ax.set_ylim(0, 8), ax.set_yticks(np.arange(1, 8))
 ax.grid(linewidth=0.125)
 plt.savefig("../figures/basic-scatter.pdf")
 ax.clear()
@@ -58,23 +58,22 @@ X = 0.5 + np.arange(8)
 Y = np.random.uniform(2, 7, len(X))
 ax.bar(X, Y, bottom=0, width=1,
        edgecolor="white", facecolor="C1", linewidth=0.25)
-ax.set_xlim(0, 8), ax.set_xticks(np.arange(1,8))
-ax.set_ylim(0, 8), ax.set_yticks(np.arange(1,8))
+ax.set_xlim(0, 8), ax.set_xticks(np.arange(1, 8))
+ax.set_ylim(0, 8), ax.set_yticks(np.arange(1, 8))
 ax.set_axisbelow(True)
 ax.grid(linewidth=0.125)
 plt.savefig("../figures/basic-bar.pdf")
 ax.clear()
 
-
 # Basic imshow plot
 # -----------------------------------------------------------------------------
 np.random.seed(3)
-I = np.zeros((8,8,4))
+I = np.zeros((8, 8, 4))
 I[:,:] = mpl.colors.to_rgba("C1")
-I[...,3] = np.random.uniform(0.25,1.0,(8,8))
-ax.imshow(I, extent=[0,8,0,8], interpolation="nearest")
-ax.set_xlim(0, 8), ax.set_xticks(np.arange(1,8))
-ax.set_ylim(0, 8), ax.set_yticks(np.arange(1,8))
+I[...,3] = np.random.uniform(0.25, 1.0, (8, 8))
+ax.imshow(I, extent=[0, 8, 0, 8], interpolation="nearest")
+ax.set_xlim(0, 8), ax.set_xticks(np.arange(1, 8))
+ax.set_ylim(0, 8), ax.set_yticks(np.arange(1, 8))
 ax.grid(linewidth=0.25, color="white")
 plt.savefig("../figures/basic-imshow.pdf")
 ax.clear()
@@ -83,50 +82,48 @@ ax.clear()
 # -----------------------------------------------------------------------------
 np.random.seed(1)
 X, Y = np.meshgrid(np.linspace(-3, 3, 256), np.linspace(-3, 3, 256))
-Z = (1 - X/2. + X**5 + Y**3)*np.exp(-X**2-Y**2)
+Z = (1 - X/2. + X**5 + Y**3) * np.exp(-X**2 - Y**2)
 Z = Z - Z.min()
 plt.pcolormesh(X, Y, Z, cmap='Oranges', shading='auto')
 ax.set_xlim(-3, 3), ax.set_xticks(np.arange(-3, 4))
-ax.set_ylim(-3, 3), ax.set_yticks(np.arange(-3,4))
+ax.set_ylim(-3, 3), ax.set_yticks(np.arange(-3, 4))
 plt.savefig("../figures/basic-pcolormesh.pdf")
 ax.clear()
 
 # Basic contour plot
 # -----------------------------------------------------------------------------
-colors = np.zeros((5,4))
+colors = np.zeros((5, 4))
 colors[:] = mpl.colors.to_rgba("C1")
-colors[:,3] = np.linspace(0.15, 0.85, len(colors))
-plt.contourf(Z, len(colors), extent=[0,8,0,8], colors=colors)
-plt.contour(Z, len(colors), extent=[0,8,0,8], colors="white", linewidths=0.125,
-            nchunk=10)
-ax.set_xlim(0, 8), ax.set_xticks(np.arange(1,8))
-ax.set_ylim(0, 8), ax.set_yticks(np.arange(1,8))
+colors[:, 3] = np.linspace(0.15, 0.85, len(colors))
+plt.contourf(Z, len(colors), extent=[0, 8, 0, 8], colors=colors)
+plt.contour(Z, len(colors), extent=[0, 8, 0, 8], colors="white",
+            linewidths=0.125, nchunk=10)
+ax.set_xlim(0, 8), ax.set_xticks(np.arange(1, 8))
+ax.set_ylim(0, 8), ax.set_yticks(np.arange(1, 8))
 plt.savefig("../figures/basic-contour.pdf")
 ax.clear()
 
-
 # Basic pie plot
 # -----------------------------------------------------------------------------
-X = 1,2,3,4
-colors = np.zeros((len(X),4))
+X = [1, 2, 3, 4]
+colors = np.zeros((len(X), 4))
 colors[:] = mpl.colors.to_rgba("C1")
-colors[:,3] = np.linspace(0.25, 0.75, len(X))
-ax.set_xlim(0, 8), ax.set_xticks(np.arange(1,8))
-ax.set_ylim(0, 8), ax.set_yticks(np.arange(1,8))
+colors[:, 3] = np.linspace(0.25, 0.75, len(X))
+ax.set_xlim(0, 8), ax.set_xticks(np.arange(1, 8))
+ax.set_ylim(0, 8), ax.set_yticks(np.arange(1, 8))
 ax.set_axisbelow(True)
 ax.grid(linewidth=0.25, color="0.75")
-ax.pie(X, colors=["white",]*len(X), radius=3, center=(4,4),
-        wedgeprops = {"linewidth": 0.25, "edgecolor": "white"}, frame=True)
-ax.pie(X, colors=colors, radius=3, center=(4,4),
-        wedgeprops = {"linewidth": 0.25, "edgecolor": "white"}, frame=True)
+ax.pie(X, colors=["white"] * len(X), radius=3, center=(4, 4),
+       wedgeprops={"linewidth": 0.25, "edgecolor": "white"}, frame=True)
+ax.pie(X, colors=colors, radius=3, center=(4, 4),
+       wedgeprops={"linewidth": 0.25, "edgecolor": "white"}, frame=True)
 plt.savefig("../figures/basic-pie.pdf")
 ax.clear()
 
-
 # Basic text plot
 # -----------------------------------------------------------------------------
-ax.set_xlim(0, 8), ax.set_xticks(np.arange(1,8))
-ax.set_ylim(0, 8), ax.set_yticks(np.arange(1,8))
+ax.set_xlim(0, 8), ax.set_xticks(np.arange(1, 8))
+ax.set_ylim(0, 8), ax.set_yticks(np.arange(1, 8))
 ax.set_axisbelow(True)
 ax.grid(linewidth=0.25, color="0.75")
 ax.text(4, 4, "TEXT", color="C1", size=8, weight="bold",
@@ -142,8 +139,8 @@ Y1 = 3 + 4*X/8 + np.random.uniform(0.0, 0.5, len(X))
 Y2 = 1 + 2*X/8 + np.random.uniform(0.0, 0.5, len(X))
 plt.fill_between(X, Y1, Y2, color="C1", alpha=.5, linewidth=0)
 plt.plot(X, (Y1+Y2)/2, color="C1", linewidth=0.5)
-ax.set_xlim(0, 8), ax.set_xticks(np.arange(1,8))
-ax.set_ylim(0, 8), ax.set_yticks(np.arange(1,8))
+ax.set_xlim(0, 8), ax.set_xticks(np.arange(1, 8))
+ax.set_ylim(0, 8), ax.set_yticks(np.arange(1, 8))
 ax.set_axisbelow(True)
 ax.grid(linewidth=0.125, color="0.75")
 plt.savefig("../figures/basic-fill.pdf")
@@ -153,12 +150,12 @@ ax.clear()
 # -----------------------------------------------------------------------------
 np.random.seed(1)
 T = np.linspace(0, 2*np.pi, 8)
-X, Y = 4 + 1*np.cos(T), 4 + 1*np.sin(T)
+X, Y = 4 + np.cos(T), 4 + np.sin(T)
 U, V = 1.5*np.cos(T), 1.5*np.sin(T)
 plt.quiver(X, Y, U, V, color="C1",
            angles='xy', scale_units='xy', scale=0.5, width=.05)
-ax.set_xlim(0, 8), ax.set_xticks(np.arange(1,8))
-ax.set_ylim(0, 8), ax.set_yticks(np.arange(1,8))
+ax.set_xlim(0, 8), ax.set_xticks(np.arange(1, 8))
+ax.set_ylim(0, 8), ax.set_yticks(np.arange(1, 8))
 ax.set_axisbelow(True)
 ax.grid(linewidth=0.125, color="0.75")
 plt.savefig("../figures/basic-quiver.pdf")
