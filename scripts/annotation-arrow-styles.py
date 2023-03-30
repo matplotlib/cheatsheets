c44d32b93ff91de6f6335f4ae91acab7cd1ebc9f
Jamal Mustafa <jimustafa@gmail.com>
2023-03-30 01:14:05 -0700

    revert some changes

--- scripts/annotation-arrow-styles.py
+++ scripts/annotation-arrow-styles.py
@@ -1,17 +1,16 @@
-import pathlib
-
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
-
-mpl.style.use([
-    pathlib.Path(__file__).parent/'../styles/base.mplstyle',
-    pathlib.Path(__file__).parent/'../styles/plotlet-grid.mplstyle',
-])
+styles = mpatches.ArrowStyle.get_styles()
 
 
-(fig, axes) = plt.subplots(4, 4, figsize=(5.7/2.54, 1.5), frameon=True)
+def demo_con_style(ax, connectionstyle):
+    ax.text(.05, .95, connectionstyle.replace(",", ",\n"),
+            family="Source Code Pro",
+            transform=ax.transAxes, ha="left", va="top", size="x-small")
+
+
+(fig, axes) = plt.subplots(4, 4, figsize=(4, 2.5), frameon=False)
 for ax in axes.flatten():
     ax.axis("off")
 for i, (ax, style) in enumerate(zip(axes.flatten(), mpatches.ArrowStyle.get_styles())):
@@ -22,13 +21,13 @@ for i, (ax, style) in enumerate(zip(axes.flatten(), mpatches.ArrowStyle.get_styl
                 xy=(x0, y0), xycoords='data',
                 xytext=(x1, y1), textcoords='data',
                 arrowprops=dict(arrowstyle=style,
-                                mutation_scale=10,
                                 color="black",
                                 shrinkA=5, shrinkB=5,
                                 patchA=None, patchB=None,
                                 connectionstyle="arc3,rad=0"))
     ax.text( (x1+x0)/2, y0-0.2, style,
              transform=ax.transAxes,
-             ha="center", va="top", fontsize=8)
+             family="Source Code Pro", ha="center", va="top")
 
 plt.savefig("../figures/annotation-arrow-styles.pdf")
+# plt.show()
--- scripts/tick-formatters.py
+++ scripts/tick-formatters.py
@@ -3,84 +3,109 @@
 # Author:  Nicolas P. Rougier
 # License: BSD
 # ----------------------------------------------------------------------------
-import pathlib
-
-import numpy as np
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
-
-mpl.style.use([
-    pathlib.Path(__file__).parent/'../styles/base.mplstyle',
-    pathlib.Path(__file__).parent/'../styles/ticks.mplstyle',
-])
+# Setup a plot such that only the bottom spine is shown
 
 
-subplots_kw = dict(
-    figsize=(5.7/2.54, 3.5/2.54),
-    nrows=7,
-    subplot_kw=dict(xlim=(0, 5), ylim=(0, 1))
-)
+def setup(ax):
+    """Set up Axes with just an x-Axis."""
+    ax.spines['right'].set_color('none')
+    ax.spines['left'].set_color('none')
+    ax.yaxis.set_major_locator(ticker.NullLocator())
+    ax.spines['top'].set_color('none')
+    ax.xaxis.set_ticks_position('bottom')
+    ax.tick_params(which='major', width=1.00, length=5)
+    ax.tick_params(which='minor', width=0.75, length=2.5, labelsize=10)
+    ax.set_xlim(0, 5)
+    ax.set_ylim(0, 1)
+    ax.patch.set_alpha(0.0)
 
-(fig, axs) = plt.subplots(**subplots_kw)
+
+fig = plt.figure(figsize=(8, 5))
+fig.patch.set_alpha(0.0)
+n = 7
+
+fontsize = 18
+family = "Source Code Pro"
 
 # Null formatter
-ax = axs[0]
+ax = fig.add_subplot(n, 1, 1)
+setup(ax)
 ax.xaxis.set_major_locator(ticker.MultipleLocator(1.00))
 ax.xaxis.set_minor_locator(ticker.MultipleLocator(0.25))
 ax.xaxis.set_major_formatter(ticker.NullFormatter())
 ax.xaxis.set_minor_formatter(ticker.NullFormatter())
-ax.text(0.0, 0.2, "ticker.NullFormatter()",
-        fontfamily="Source Code Pro", transform=ax.transAxes, in_layout=False)
+ax.text(0.0, 0.1, "ticker.NullFormatter()", family=family,
+        fontsize=fontsize, transform=ax.transAxes)
 
 # Fixed formatter
-ax = axs[1]
+ax = fig.add_subplot(n, 1, 2)
+setup(ax)
 ax.xaxis.set_minor_locator(ticker.MultipleLocator(0.25))
 ax.xaxis.set_major_locator(ticker.FixedLocator(range(6)))
 majors = ["zero", "one", "two", "three", "four", "five"]
 ax.xaxis.set_major_formatter(ticker.FixedFormatter(majors))
-ax.text(0.0, 0.2, "ticker.FixedFormatter(['zero', 'one', 'two', …])",
-        fontfamily="Source Code Pro", transform=ax.transAxes, in_layout=False)
+ax.text(0.0, 0.1, "ticker.FixedFormatter(['zero', 'one', 'two', …])",
+        family=family, fontsize=fontsize, transform=ax.transAxes)
 
-# FuncFormatter formatter
-ax = axs[2]
+
+# FuncFormatter can be used as a decorator
+@ticker.FuncFormatter
+def major_formatter(x, pos):
+    """Return formatted value with 2 decimal places."""
+    return "[%.2f]" % x
+
+
+ax = fig.add_subplot(n, 1, 3)
+setup(ax)
 ax.xaxis.set_major_locator(ticker.MultipleLocator(1.00))
 ax.xaxis.set_minor_locator(ticker.MultipleLocator(0.25))
-ax.xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: "[%.2f]" % x))
-ax.text(0.0, 0.2, 'ticker.FuncFormatter(lambda x, pos: "[%.2f]" % x)',
-        fontfamily="Source Code Pro", transform=ax.transAxes, in_layout=False)
+ax.xaxis.set_major_formatter(major_formatter)
+ax.text(0.0, 0.1, 'ticker.FuncFormatter(lambda x, pos: "[%.2f]" % x)',
+        family=family, fontsize=fontsize, transform=ax.transAxes)
+
 
 # FormatStr formatter
-ax = axs[3]
+ax = fig.add_subplot(n, 1, 4)
+setup(ax)
 ax.xaxis.set_major_locator(ticker.MultipleLocator(1.00))
 ax.xaxis.set_minor_locator(ticker.MultipleLocator(0.25))
 ax.xaxis.set_major_formatter(ticker.FormatStrFormatter(">%d<"))
-ax.text(0.0, 0.2, "ticker.FormatStrFormatter('>%d<')",
-        fontfamily="Source Code Pro", transform=ax.transAxes, in_layout=False)
+ax.text(0.0, 0.1, "ticker.FormatStrFormatter('>%d<')",
+        family=family, fontsize=fontsize, transform=ax.transAxes)
 
 # Scalar formatter
-ax = axs[4]
+ax = fig.add_subplot(n, 1, 5)
+setup(ax)
 ax.xaxis.set_major_locator(ticker.AutoLocator())
 ax.xaxis.set_minor_locator(ticker.AutoMinorLocator())
 ax.xaxis.set_major_formatter(ticker.ScalarFormatter(useMathText=True))
-ax.text(0.0, 0.2, "ticker.ScalarFormatter()",
-        fontfamily="Source Code Pro", transform=ax.transAxes, in_layout=False)
+ax.text(0.0, 0.1, "ticker.ScalarFormatter()",
+        family=family, fontsize=fontsize, transform=ax.transAxes)
 
 # StrMethod formatter
-ax = axs[5]
+ax = fig.add_subplot(n, 1, 6)
+setup(ax)
 ax.xaxis.set_major_locator(ticker.MultipleLocator(1.00))
 ax.xaxis.set_minor_locator(ticker.MultipleLocator(0.25))
 ax.xaxis.set_major_formatter(ticker.StrMethodFormatter("{x}"))
-ax.text(0.0, 0.2, "ticker.StrMethodFormatter('{x}')",
-        fontfamily="Source Code Pro", transform=ax.transAxes, in_layout=False)
+ax.text(0.0, 0.1, "ticker.StrMethodFormatter('{x}')",
+        family=family, fontsize=fontsize, transform=ax.transAxes)
 
 # Percent formatter
-ax = axs[6]
+ax = fig.add_subplot(n, 1, 7)
+setup(ax)
 ax.xaxis.set_major_locator(ticker.MultipleLocator(1.00))
 ax.xaxis.set_minor_locator(ticker.MultipleLocator(0.25))
 ax.xaxis.set_major_formatter(ticker.PercentFormatter(xmax=5))
-ax.text(0.0, 0.2, "ticker.PercentFormatter(xmax=5)",
-        fontfamily="Source Code Pro", transform=ax.transAxes, in_layout=False)
+ax.text(0.0, 0.1, "ticker.PercentFormatter(xmax=5)",
+        family=family, fontsize=fontsize, transform=ax.transAxes)
 
-fig.savefig("../figures/tick-formatters.pdf")
+# Push the top of the top axes outside the figure because we only show the
+# bottom spine.
+fig.subplots_adjust(left=0.05, right=0.95, bottom=0.05, top=1.05)
+
+plt.savefig("../figures/tick-formatters.pdf", transparent=True)
+# plt.show()
