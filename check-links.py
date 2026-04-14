43ab421bd472d2ce22c97772eae19e5e43f80ec9
David Stansby <dstansby@gmail.com>
2026-04-14 14:36:31 +0100

    Fix link checking

--- check-links.py
+++ check-links.py
@@ -12,7 +12,7 @@ status_codes = [pdfx.downloader.get_status_code(ref.ref) for ref in refs]
 
 broken_links = [(ref.ref, code) for ref, code in zip(refs, status_codes) if code != 200]
 # It seems that stackoverflow does not respond well to the link checker and throws a 400
-broken_links = [b for b in broken_links if not 'stackoverflow.com' in b[0]]
+broken_links = [b for b in broken_links if not ('stackoverflow.com' in b[0])]
 
 if len(broken_links) == 0:
     sys.exit(0)
