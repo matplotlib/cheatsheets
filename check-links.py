d4a879f071e06b44b421c51f8205ca490eff8eb5
David Stansby <dstansby@gmail.com>
2026-04-16 06:08:01 +0200

    Update getting help section (#186)
    This updates the getting help section to mirror the current resoures that are advertised on https://matplotlib.org/

--- check-links.py
+++ check-links.py
@@ -11,8 +11,8 @@ refs = [ref for ref in pdf.get_references() if ref.reftype == 'url']
 status_codes = [pdfx.downloader.get_status_code(ref.ref) for ref in refs]
 
 broken_links = [(ref.ref, code) for ref, code in zip(refs, status_codes) if code != 200]
-# It seems that Twitter and stackoverflow do not respond well to the link checker and throws a 400
-broken_links = [b for b in broken_links if not ('twitter.com' in b[0] or 'stackoverflow.com' in b[0])]
+# It seems that stackoverflow does not respond well to the link checker and throws a 400
+broken_links = [b for b in broken_links if not ('stackoverflow.com' in b[0])]
 
 if len(broken_links) == 0:
     sys.exit(0)
